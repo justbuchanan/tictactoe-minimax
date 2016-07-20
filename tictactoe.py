a9011386096ecc59e89ec0d92fbaf7e019bc6f51
Justin Buchanan <justbuchanan@gmail.com>
2016-07-20 18:24:28 -0400

    making progress

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,7 @@
 from enum import Enum
 import numpy as np
 
-class Square(enum):
+class Square(Enum):
     EMPTY = 0
     O = 1
     X = 2
@@ -53,59 +53,41 @@ class Board:
     def done(self):
         return self.winner() != None
 
+    def __str__(self):
+        return '\n'.join([' '.join(r) for r in self.rows()])
 
 
-def print_grid(grid):
-    print('-'*5)
-    for row in grid:
-        print('|'.join(row))
-    print('-'*5)
-
-# @param sq An (r, c) tuple
-def set_square(grid, sq, smbl):
-    if grid[sq[0]][sq[1]] != ' ':
-        raise RuntimeError("Error: squre '%s' already claimed" % str(sq))
-    grid[sq[0]][sq[1]] = smbl
-
-
-def get_col(grid, c):
-    return [grid[i][c] for i in range(3)]
-
-def get_row(grid, r):
-    return [grid[r][i] for i in range(3)]
-
-
-def game_winner(grid):
-    for c in range(3):
-        
+class InvalidMove(RuntimeError): pass
 
 
 def run_game(player1, player2):
-    grid = [
-        [' ',' ',' '],
-        [' ',' ',' '],
-        [' ',' ',' ']
-    ]
+    brd = Board()
 
     players = [player1, player2]
-    symbols = ['x', 'o']
     count = 0
-    while game_winner(grid) == None:
+    while (grid) == None:
         p = players[count % len(players)]
-        s = symbols[count % len(players)]
         count += 1
-        move = p(grid)
-        set_square(grid, move, s)
-        print_grid(grid)
-        print()
+
+        s = [Square.O, Square.X][count % len(players)]
+
+        mv = p(grid)
+        brd._grid[mv] = s
+        print('\n-----')
+        print(grid)
+        print('-----\n')
 
 
 i = 0
-def player(grid):
+def auto_player(grid):
     global i
     v = (i,i)
     i += 1
     return v
 
+def console_player(grid):
+    res = raw_input('Enter choice (0-8):')
+    return int(res)
 
-run_game(player, player)
+
+run_game(auto_player, console_player)
